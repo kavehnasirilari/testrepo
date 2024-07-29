767ba2a1f43296baa59d0f676f358d658676c5ea
kaveh nasiri lari <85795023+kavehnasirilari@users.noreply.github.com>
2024-07-29 16:22:19 +0330

    Add files via upload

---
+++ testfile.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 11:18:23 2024
+
+@author: Kaveh
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+# crawling logic... 
+response = requests.get("https://books.toscrape.com/")
+
+
+
